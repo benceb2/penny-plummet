92f5d9c91f747606f3d15bf37fc9058b7d44bc69
Bence <bence.barkanyi@gmail.com>
2024-12-29 21:16:02 +0000

    Update document title format in router to place title before app name

--- src/router/index.ts
+++ src/router/index.ts
@@ -42,7 +42,7 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
   document.title = to.meta.title
-    ? `Penny Plummet | ${to.meta.title}`
+    ? `${to.meta.title} | Penny Plummet`
     : 'Penny Plummet'
   next()
 })

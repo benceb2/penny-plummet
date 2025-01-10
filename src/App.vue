e6fad07d8be06c37c92d3eaf9d5c2e10dcee26aa
Bence <bence.barkanyi@gmail.com>
2025-01-10 20:32:34 +0000

    use '@' imports in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import UsernameModal from './components/modals/UsernameModal.vue'
-import ConsentModal from './components/modals/ConsentModal.vue'
-import OfflineEarningsModal from './components/modals/OfflineEarningsModal.vue'
-import AppNavbar from './components/layout/AppNavbar.vue'
-import AppFooter from './components/layout/AppFooter.vue'
-import { useClickerStore } from './stores/clickerStore'
+import UsernameModal from '@/components/modals/UsernameModal.vue'
+import ConsentModal from '@/components/modals/ConsentModal.vue'
+import OfflineEarningsModal from '@/components/modals/OfflineEarningsModal.vue'
+import AppNavbar from '@/components/layout/AppNavbar.vue'
+import AppFooter from '@/components/layout/AppFooter.vue'
+import { useClickerStore } from '@/stores/clickerStore'
 
 const store = useClickerStore()
 

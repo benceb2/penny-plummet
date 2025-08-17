009fa84bc1265d17359f4868bae0d046d5ea8b88
Bence <bence.barkanyi@gmail.com>
2025-08-17 17:06:31 +0100

    tweak home page achievement/transaction lists layout

--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -99,13 +99,13 @@ const recentTransactions = computed(() => {
               {{ t('home.achievements.title') }}
             </h5>
           </div>
-          <div class="card-body">
+          <div class="card-body p-2">
             <div class="achievements-list">
               <AchievementCard
                 v-for="achievement in nearestAchievements"
                 :key="achievement.id"
                 :achievement="achievement"
-                class="mb-3" />
+                class="mb-2" />
             </div>
           </div>
           <div class="card-footer bg-transparent">
@@ -129,7 +129,7 @@ const recentTransactions = computed(() => {
               {{ t('home.transactions.title') }}
             </h5>
           </div>
-          <div class="card-body p-0">
+          <div class="card-body p-0 mt-1">
             <div v-if="recentTransactions.length > 0">
               <TransactionItem
                 v-for="transaction in recentTransactions"
